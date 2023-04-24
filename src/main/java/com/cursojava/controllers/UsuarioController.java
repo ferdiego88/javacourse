69254f905de7f3fbec7522a4fab122461fc15f5a
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-04-24 18:08:13 -0500

    Conectar con los properties a la BD

--- src/main/java/com/cursojava/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.cursojava.controllers;
 
+import com.cursojava.dao.UsuarioDao;
 import com.cursojava.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,9 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    
+    @Autowired
+    private UsuarioDao usuarioDao;
+
     @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario = new Usuario();
@@ -26,34 +30,7 @@ public class UsuarioController {
     @RequestMapping(value = "usuarios")
     public List<Usuario> getUsuarios() {
 
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario usuario = new Usuario();
-        usuario.setId(456L);
-        usuario.setNombre("Lucas");
-        usuario.setApellido("Santillan");
-        usuario.setEmail("fernando@gmail.com");
-        usuario.setTelefono("942215621");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(457L);
-        usuario2.setNombre("Rosa");
-        usuario2.setApellido("Varas");
-        usuario2.setEmail("rosita.varaso@gmail.com");
-        usuario2.setTelefono("98899852");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(458L);
-        usuario3.setNombre("Pablo");
-        usuario3.setApellido("Torres");
-        usuario3.setEmail("pablo@gmail.com");
-        usuario3.setTelefono("98545542");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
 
--- src/main/java/com/cursojava/models/Usuario.java
+++ src/main/java/com/cursojava/models/Usuario.java
@@ -1,25 +1,31 @@
 package com.cursojava.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Entity
 @Table(name="usuarios")
+@ToString @EqualsAndHashCode
 public class Usuario {
 
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "apellido")
     private String apellido;
+    @Column(name = "email")
     private String email;
+    @Column(name = "telefono")
     private String telefono;
+    @Column(name = "password")
     private String password;
 
 }
