78087b3325ef9ef351b75898a1dce082984c7e9d
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-04-24 16:32:04 -0500

    Agregar Anotaciones repository y transactional

--- src/main/java/com/cursojava/dao/UsuarioDaoImpl.java
+++ src/main/java/com/cursojava/dao/UsuarioDaoImpl.java
@@ -1,9 +1,13 @@
 package com.cursojava.dao;
 
 import com.cursojava.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
+@Transactional
 public class UsuarioDaoImpl implements UsuarioDao {
 
     @Override
